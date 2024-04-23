aad24abffce5170777bea5a8bc0e21ba2845e188
saad-rao <167574691+saad-rao@users.noreply.github.com>
2024-04-23 15:24:11 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env/ node
+
+import inquirer from "inquirer"
+
+const answers: {
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"enter your sentence to count the word"
+    }
+])
+
+const words = answers.sentence.trim().split(" ")
+
+console.log(words);
+
+console.log(`your sentence word count is ${words.length}`);
